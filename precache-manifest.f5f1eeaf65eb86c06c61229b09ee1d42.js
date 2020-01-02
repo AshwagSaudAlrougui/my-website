a0560fdf271ecd594aea79afe16866dd34848bb0
ASSROUGUI <ashwagooo@hotmail.com>
2020-01-02 19:54:02 +0300

    Updates

---
+++ precache-manifest.f5f1eeaf65eb86c06c61229b09ee1d42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360b0957a24c03c091ed2b1f7be19028",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "ea0671ed18a0ca81059e",
+    "url": "/my-website/static/css/2.3ee686e4.chunk.css"
+  },
+  {
+    "revision": "a85a8ded6bbbe5a3e8a2",
+    "url": "/my-website/static/css/main.7c352035.chunk.css"
+  },
+  {
+    "revision": "ea0671ed18a0ca81059e",
+    "url": "/my-website/static/js/2.5f29537a.chunk.js"
+  },
+  {
+    "revision": "4f8842d2c5ede7d4c10b15e150425a58",
+    "url": "/my-website/static/js/2.5f29537a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a85a8ded6bbbe5a3e8a2",
+    "url": "/my-website/static/js/main.15d267f7.chunk.js"
+  },
+  {
+    "revision": "cdd1e0d4b7c0bf48a702",
+    "url": "/my-website/static/js/runtime-main.5976af35.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
